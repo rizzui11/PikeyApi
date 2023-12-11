696eb01c76121c3bfbe86e41bf90cccbc2103690
rizzui11 <153467330+rizzui11@users.noreply.github.com>
2023-12-11 07:06:53 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://rizzihaha5:RIZZISHU123@cluster0.vry1iim.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "rizzihaha5@gmail.com" //email
+global.email_password = "evau elhs cszw atla" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
